8f297f1776b6e99a1dd037ef307941678b6615f1
johnadapt <2372574+johnadapt@users.noreply.github.com>
2025-09-18 14:28:42 -0700

    quick fix

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,6 +3,6 @@
 // Register backend module under Accounts → Member Import
 $GLOBALS['BE_MOD']['accounts']['bcs_member_import'] = [
     'tables' => [],
-    'callback' => \\Bcs\\MemberImport\\Module\\MemberImport::class,
+    'callback' => \Bcs\MemberImport\Module\MemberImport::class,
     'icon' => 'system/themes/flexible/icons/members.svg'
 ];
